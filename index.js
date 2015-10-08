a7fe1fadafff46ea429914e9f6e8293b69edc02e
zool <zool.bib@gmail.com>
2015-10-08 11:09:27 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,34 @@
+// dependencies for mnml
+
+var fs = require("fs")
+var autoprefixer = require("autoprefixer")
+var postcss = require("postcss")
+var atImport = require("postcss-import")
+var cssvariables = require('postcss-css-variables')
+var compressor = require('node-minify')
+var conditionals = require('postcss-conditionals')
+var customMedia = require("postcss-custom-media")
+
+// css to be processed
+var css = fs.readFileSync("src/basscss.css", "utf8")
+
+// process css
+var output = postcss([autoprefixer])
+  .use(atImport())
+  .use(cssvariables())
+  .use(conditionals())
+  .use(customMedia())
+  .process(css, {
+    from: "./src/basscss.css",
+    to: "./css/s.css"
+  })
+  .css
+
+fs.writeFile("css/s.css", output, 'utf-8')
+
+// Using Sqwish for CSS
+new compressor.minify({
+    type: 'sqwish',
+    fileIn: './css/s.css',
+    fileOut: './css/s.min.css'
+});
